cdbe5c4c42ae36baaeb8b081f848b1d9b4d9a30a
Xenia <listk98@yandex.ru>
2022-03-30 23:49:29 +0300

    Add Task_1.1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -4,48 +4,49 @@ int N = Promt("Введите длину массива: ");
 
 if (N > 0)
 {
-	int[] array = RandomArray(N);	
+    int[] array = RandomArray(N);	
     PrintArray(array);
     Console.WriteLine($"Количество четных чисел в массиве составляет {FindCout(array)}");
 }
+
 else Console.WriteLine("Введено некорректное значение длины массива");
 
 
 
 int Promt(String message)
 {
-	Console.Write(message);
-	string strValue = Console.ReadLine();
-	int Value = int.Parse(strValue);
+    Console.Write(message);
+    string strValue = Console.ReadLine();
+    int Value = int.Parse(strValue);
     return Value;
 }
 
 int[] RandomArray (int length)
 {
-	int[] array = new int[length];
-	for (int i = 0; i < length; i++)
-	{
-		array[i] = new Random().Next(100,1000);
-	}
-	return array;
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(100,1000);
+    }
+    return array;
 }
 
 void PrintArray(int[] array)
 {
-	for (int i = 0; i < array.Length; i++)
-	{
-		Console.Write(array[i] + " ");
-	}
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
     Console.WriteLine();
 }
 
 int FindCout(int[] array)
 {
-	int cout = 0;
-	for (int i = 0; i < array.Length; i++)
-	{
-		if (array[i] % 2 == 0) cout++;
-}
+    int cout = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) cout++;
+    }
     return cout;
 }
 
